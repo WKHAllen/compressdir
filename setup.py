912818c1a82828d209aae3cd6f8853026599752a
WKHAllen <wkhallen@gmail.com>
2019-05-16 15:00:02 -0500

    Fix long description

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
+import os
 from setuptools import setup
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name = 'compressdir',
     packages = ['compressdir'],
-    version = '0.1.0',
+    version = '0.1.2',
     license = 'MIT',
-    description = 'File/Directory compression',
-    long_description = long_description,
+    description = 'File/directory compression',
+    long_description = ''.join(open('README.md', encoding='utf-8').readlines()),
+    long_description_content_type = 'text/markdown',
     author = 'Will Allen',
     author_email = 'wkhallen@gmail.com',
     url = 'https://github.com/WKHAllen/compressdir',
